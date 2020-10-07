2066eb5d8667760e1215587cc1a70fbf6fbfc4a2
Gabriel Santos <barricaspt@gmail.com>
2020-10-07 17:33:23 +0100

    Remove unused dependency

--- src/teamspeak/server-events.service.ts
+++ src/teamspeak/server-events.service.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { TeamspeakBusService } from './teamspeak-bus.service';
 import { crawlConfigUpdatedEventSubject } from './subjects';
-import { ZoneService } from '../servers/configs/zone/zone.service';
 import { CrawlerConfiguration } from './types/crawl';
 
 @Injectable()
 export class TsServerEventsService {
-  constructor(
-    private busService: TeamspeakBusService,
-    private zoneService: ZoneService,
-  ) {}
+  constructor(private busService: TeamspeakBusService) {}
 
   /**
    * Emit an event with the updated zone
