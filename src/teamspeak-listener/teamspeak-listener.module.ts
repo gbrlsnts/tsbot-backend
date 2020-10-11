1d0d98821fdb7ee3d47b473df88428e42f3738b6
Gabriel Santos <barricaspt@gmail.com>
2020-10-11 04:06:07 +0100

    bot provider not needed within teamspeak listener module

--- src/teamspeak-listener/teamspeak-listener.module.ts
+++ src/teamspeak-listener/teamspeak-listener.module.ts
@@ -3,11 +3,10 @@ import { ConfigListenerService } from './config.service';
 import { ServersModule } from '../servers/servers.module';
 import { ConfigListenerController } from './config.controller';
 import { TeamspeakCommonModule } from '../teamspeak-common/teamspeak-common.module';
-import { botProvider } from '../shared/nats/ts-bot.provider';
 
 @Module({
   imports: [ServersModule, TeamspeakCommonModule],
-  providers: [botProvider, ConfigListenerService],
+  providers: [ConfigListenerService],
   controllers: [ConfigListenerController],
 })
 export class TeamspeakListenerModule {}
